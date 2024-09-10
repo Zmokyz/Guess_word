4351edd065d565d0896b561240858d5715113b0c
Zmokyz <Ortizzarif2@gmail.com>
2024-09-10 05:46:02 -0600

    added function to te inputs and make the word reversed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,38 @@
-function Randomwrd(){
-    let Randwrd = wordList[Math.floor(Math.random() * wordList.length)];
-    let Word =Randwrd.word;
-    console.log(Word)
 
+const initGame = () => {
+    let randomObj = wordList[Math.floor(Math.random() * wordList.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    let prepWord = wordArray.join("")
+    const Word_show = document.getElementById("Word_show")
+    Word_show.placeholder = prepWord
 }
+initGame();
 
-Randomwrd()
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const inputs = document.querySelectorAll('.text_show');
+  
+    inputs.forEach((input, index) => {
+      input.addEventListener('input', function() {
+        if (this.value.length >= this.maxLength) {
+          if (index < inputs.length - 1) {
+            inputs[index + 1].focus();
+          }
+        }
+      });
+  
+      input.addEventListener('keydown', function(e) {
+        if (e.key === 'Backspace' && this.value.length === 0) {
+          if (index > 0) {
+            inputs[index - 1].focus();
+          }
+        }
+      });
+    });
+  });
